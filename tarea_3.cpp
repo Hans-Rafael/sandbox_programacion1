4ab1f920d858de6d6713f57b879e300dd099815c
Your Name <your.email@example.com>
2026-04-09 13:27:27 -0300

    doc: agregada informacion

--- tarea_3.cpp
+++ tarea_3.cpp
@@ -4,3 +4,10 @@
 void ejecutarTarea3() {
     std::cout << "Hola soy Tarea 3 La tarea debe ejecutarse aqui entre las llaves amarillas\n";
 }
+// Main de prueba (borrar después)
+/*
+int main() {
+    ejecutarTarea1();
+    return 0;
+}
+*/
